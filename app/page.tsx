03ae3c55938bf18d31f52473d34e524b6440d4ea
maitymohar <maitymohar2005@gmail.com>
2024-09-13 01:59:18 +0530

    Checking the funcationality of the Platform

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button";
 
 
 export default function Home() {
   return (
-   <p className="text-6xl">Hello World</p>
+    <>
+    <Button>Click Me</Button>
+   
+    </>
+    
   );
 }
